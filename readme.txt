9843ea2221703c523f415a2d1f75915416803a7c
blogger323 <blogger323@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-10 23:39:34 +0000

    Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/thin-out-revisions/trunk@623497 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://en.hetarena.com/donate
 Tags: revisions, revision, posts, admin
 Requires at least: 3.2
 Tested up to: 3.4.2
-Stable tag: 0.8
+Stable tag: 1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -13,21 +13,22 @@ Thin Out Revisions is a plugin to thin out post/page revisions manually.
 == Description ==
 
 As its default behavior, WordPress always makes a new revision when you save your post. 
+In addition, this also happens when you do a preview because you implicitly save your post when you press the preview button.
 This is too often even if you like revision control. Thin Out Revisions (TOR), a plugin 
 for WordPress, will help you thin out revisions manually.
 
 After activating this plugin, you will see a button to thin out revisions below the table 
-on revision.php. To thin out, simply click the button.
+on revision.php, where you compare revisions. To thin out, simply click the button.
 
 If you have selected revisions having intermediate revisions between them, TOR button is to remove the intermediates. 
 Or if you have selected revisions next to each other, TOR button is to remove the older selected one. 
 To thin out intermediates between selected revisions is very intuitive operation and easy to use. 
-But you may also want to remove the oldest revision, so I have made these two behaviors. 
+But you may also want to remove the oldest revision (somtimes a enpty revision!), so I have made these two behaviors. 
 Please carefully check a message on the button and displayed revisions to remove.
 
-TOR uses a standard function, wp_delete_post_revision, to remove revisions. 
-So, it also removes related data and works fine in multisite environment. 
-If you like revision control, this is MUST HAVE. Happy blogging!
+TOR uses a standard function, wp_delete_post_revision, to delete revisions. 
+So, it also deletes related data and works fine in multisite environment. 
+If you like revision control, you will like it. Download TOR today for your happy blogging.
 
 Related Links:
 
@@ -38,26 +39,42 @@ Related Links:
 You can install Thin Out Revisions in a standard manner.
 
 1. Go to Plugins - Add New page of your WordPress and install Thin Out Revisions plugin.
-1. Or unzip `thin-out-revisions.zip` to your `/wp-content/plugins/` directory.
+1. Or unzip `thin-out-revisions.*.zip` to your `/wp-content/plugins/` directory. The asterisk is a version number.
 
 Thin Out Revisions comes with no setting options.
 Don't forget to activate the plugin before use it.
 
 == Frequently Asked Questions ==
 
-This section will be updated soon.
+= I can't see any changes after an activation of the plugin. =
+Change is only a new button on revision.php.
+
+= Where is revision.php? =
+You can go to revision.php by choosing a revision on 'Edit Post'/'Edit Page' screen.
+If you can't see any revisions on the 'Edit Post'/'Edit Page' screen, check the 'Revisions' option in Screen Options at top-right of the page.
+If you can't see the 'Revisions' option in the menu, I guess you still don't have any revisions. So edit post/page and save it first.
 
 == Screenshots ==
 
-1. A Button to remove intermediate revisions
-2. A Button to remove a single revision
+1. A button to remove intermediate revisions
+2. A button to remove a single revision
+3. A screen after deleting some revisions
 
 == Changelog ==
+
+= 1.0 =
+* The version number is now 1.0. Though it's not a substantial change, many people will feel it sounds very formal.
+* Better visual effects on deleted revisions. Now you can easily identify them.
+* [Bug fix] The number of deleted revisions is corrected to show a real deleted number.
+* Now the code is using a class. No impact for end users.
+* Other some minor changes for stable operation.
 
 = 0.8 =
 * The first version introduced in wordpress.org repository.
 
 == Upgrade Notice ==
 
-This section will be updated when a new release comes.
+= 1.0 =
+Better visual effects on deleted revisions. Now you can easily identify them.
+For more information, check the change log on the plugin page.
 
--- thin-out-revisions.php
+++ thin-out-revisions.php
@@ -3,101 +3,108 @@
 Plugin Name: Thin Out Revisions
 Plugin URI: http://en.hetarena.com/thin-out-revisions
 Description: A plugin to thin out post/page revisions manually. 
-Version: 0.8
+Version: 1.0
 Author: Hirokazu Matsui
 Author URI: http://en.hetarena.com/
 License: GPLv2
   */
 
-define( 'HM_TOR_VERSION', '0.8' );
+define( 'HM_TOR_VERSION', '1.0' );
 
-register_activation_hook( __FILE__, 'hm_tor_install' );
 
-add_action( 'init', 'hm_tor_init' );
-add_action( 'admin_head', 'hm_tor_message', 20 );
-add_action( 'wp_enqueue_scripts', 'hm_tor_scripts', 20);
-add_action( 'wp_ajax_hm_tor_do_ajax', 'hm_tor_do_ajax' );
 
-function hm_tor_init() {
-  load_plugin_textdomain( 'thin-out-revisions', false, 'thin-out-revisions/languages' );
-}
-
-function hm_tor_install() {
-  if ( version_compare( get_bloginfo( 'version' ), '3.2', '<' ) ) {
-    deactivate_plugins( basename( __FILE__ ) ); // Deactivate our plugin
-  }
-}
-
-function hm_tor_scripts() {
-  wp_enqueue_script( 'jquery' );
-}
-
-function hm_tor_message() {
-
-  global $left, $right, $post;
-
-  $uri = parse_url( $_SERVER['REQUEST_URI'] );  
-
-  if ( strpos( $uri['path'], '/revision.php' ) === false ) {
-    return;
+class HM_TOR_Plugin_Loader {
+  function __construct() {
+    register_activation_hook( __FILE__,   array( &$this, 'hm_tor_install' ) );
+    add_action( 'plugins_loaded',         array( &$this, 'hm_tor_loaded' ) );
+    add_action( 'admin_head',             array( &$this, 'hm_tor_message' ), 20 );
+    add_action( 'wp_enqueue_scripts',     array( &$this, 'hm_tor_scripts' ), 20);
+    add_action( 'wp_ajax_hm_tor_do_ajax', array( &$this, 'hm_tor_do_ajax' ) );
   }
 
-  if ( !$revisions = wp_get_post_revisions( $post->ID ) )
-    return;
+  function hm_tor_loaded() {
+    load_plugin_textdomain( 'thin-out-revisions', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+  }
 
-  if ( !( $left != '' && $right != '' && $left != $right ) )
-    return;
-
-  array_unshift( $revisions, $post );
-
-  $liststr = "<ul>";
-  $in_range = false;
-  $post_notselected = false;
-  $posts2del = array();
-  foreach ( $revisions as $revision ) {
-    if ( $revision->ID == $right ) {
-      $in_range = true;
-    }
-    else if ( $revision->ID == $left ) {
-      $in_range = false;
-    }
-    else if ( $in_range ) {
-      $post_notselected = true;
-      if ( current_user_can( 'edit_post', $revision->ID ) ) {
-        $liststr .= '<li>&quot;' . wp_post_revision_title( $revision, false ) . "&quot;</li>";
-        array_unshift( $posts2del, $revision->ID );
-      }
+  function hm_tor_install() {
+    if ( version_compare( get_bloginfo( 'version' ), '3.2', '<' ) ) {
+      deactivate_plugins( basename( __FILE__ ) ); // Deactivate this plugin
     }
   }
-  $liststr .= "</ul>";
 
-  if ( count( $posts2del ) == 0 ) {
-    if ( !$post_notselected && current_user_can( 'edit_post', $left ) ) {
-      $buttonval  = sprintf( __( "Remove a revision &quot;%s&quot;", 'thin-out-revisions' ), wp_post_revision_title( $left, false ) );
-      $posts_data = $left;
-      $msg_reload = sprintf( __( "Once you return <a href=\"%s\">post-edit page</a>, you could continue working with revisions. Or reselect existing revisions and press &quot;%s&quot; button.", 'thin-out-revisions' ), get_edit_post_link( $post->ID ), __( "Compare Revisions" ) );
-    }
-    else {
+  function hm_tor_scripts() {
+    wp_enqueue_script( 'jquery' );
+  }
+
+  function hm_tor_message() {
+
+    global $left, $right, $post;
+
+    $uri = parse_url( $_SERVER['REQUEST_URI'] );  
+
+    if ( strpos( $uri['path'], '/revision.php' ) === false ) {
       return;
     }
-  }
-  else {
-    $buttonval = sprintf( __( "Remove revisions between &quot;%s&quot; and &quot;%s&quot; exclusively", 'thin-out-revisions' ),
-      wp_post_revision_title( $right, false ), wp_post_revision_title( $left, false ) );
-    $posts_data = implode( '-', $posts2del);
-    $msg_reload = __( "To update the compare-revision table, reload this page.", 'thin-out-revisions' );
-  }
-  $ajaxnonce = wp_create_nonce( 'hm-rto-delete-' . $posts_data );  
-  $ajaxurl = admin_url( 'admin-ajax.php', isset( $_SERVER["HTTPS"] ) ? 'https' : 'http' );
 
-  $msg_confirm = __( "You really remove?", 'thin-out-revisions' );
-  $msg_process = __( 'Processing ...', 'thin-out-revisions' );
-  $msg_info    = count( $posts2del ) > 0 ? __( 'Following revisions will be removed.', 'thin-out-revisions' ) : "";
-  $msg_info2   = sprintf( __( "To change revisions to remove, you have to press &quot;%s&quot; button after selection.",'thin-out-revisions' ), __( 'Compare Revisions' ) );
-  $msg_error   = __( 'Error in communication with server', 'thin-out-revisions' );
-  $msg_title   = __( 'Thin Out Revisions', 'thin-out-revisions' );
+    if ( !$revisions = wp_get_post_revisions( $post->ID ) )
+      return;
 
-  $src = <<<JQSRC
+    if ( !( $left != '' && $right != '' && $left != $right ) )
+      return;
+
+    array_unshift( $revisions, $post );
+
+    $revs = array();
+    $liststr = "<ul>";
+    $in_range = false;
+    $post_notselected = false;
+    $posts2del = array();
+    foreach ( $revisions as $revision ) {
+      array_push( $revs, "'" . $revision->ID . "'" );
+      if ( $revision->ID == $right ) {
+        $in_range = true;
+      }
+      else if ( $revision->ID == $left ) {
+        $in_range = false;
+      }
+      else if ( $in_range ) {
+        $post_notselected = true;
+        if ( current_user_can( 'edit_post', $revision->ID ) ) {
+          $liststr .= '<li>&quot;' . wp_post_revision_title( $revision, false ) . "&quot;</li>";
+          array_push( $posts2del, $revision->ID );
+        }
+      }
+    }
+    $revs_str = "[" . implode( $revs, "," ) ."]";
+    $liststr .= "</ul>";
+
+    if ( count( $posts2del ) == 0 ) {
+      if ( !$post_notselected && current_user_can( 'edit_post', $left ) ) {
+        $buttonval  = sprintf( __( "Remove a revision &quot;%s&quot;", 'thin-out-revisions' ), wp_post_revision_title( $left, false ) );
+        $posts_data = $left;
+        $msg_reload = sprintf( __( "Once you return <a href=\"%s\">post-edit page</a>, you could continue working with revisions. Or reselect existing revisions and press &quot;%s&quot; button.", 'thin-out-revisions' ), get_edit_post_link( $post->ID ), __( "Compare Revisions" ) );
+      }
+      else {
+        return;
+      }
+    }
+    else {
+      $buttonval = sprintf( __( "Remove revisions between &quot;%s&quot; and &quot;%s&quot; exclusively", 'thin-out-revisions' ),
+        wp_post_revision_title( $right, false ), wp_post_revision_title( $left, false ) );
+      $posts_data = implode( '-', $posts2del);
+      $msg_reload = __( "To update the compare-revision table, reload this page.", 'thin-out-revisions' );
+    }
+    $ajaxnonce = wp_create_nonce( 'hm-rto-delete-' . $posts_data );  
+    $ajaxurl = admin_url( 'admin-ajax.php', isset( $_SERVER["HTTPS"] ) ? 'https' : 'http' );
+
+    $msg_confirm = __( "You really remove?", 'thin-out-revisions' );
+    $msg_process = __( 'Processing ...', 'thin-out-revisions' );
+    $msg_info    = count( $posts2del ) > 0 ? __( 'Following revisions will be removed.', 'thin-out-revisions' ) : "";
+    $msg_info2   = sprintf( __( "To change revisions to remove, you have to press &quot;%s&quot; button after selection.",'thin-out-revisions' ), __( 'Compare Revisions' ) );
+    $msg_error   = __( 'Error in communication with server', 'thin-out-revisions' );
+    $msg_title   = __( 'Thin Out Revisions', 'thin-out-revisions' );
+
+    $src = <<<JQSRC
 <script type="text/javascript">
   jQuery(document).ready(function() {
     jQuery('#wpbody-content .wrap').append(
@@ -115,6 +122,7 @@ function hm_tor_message() {
       jQuery('#hm_tor_msg').html('$msg_process');
       jQuery.ajax({
         url: '$ajaxurl',
+        dataType: 'json',
         data: {
           action: 'hm_tor_do_ajax',
           posts: '$posts_data',
@@ -122,7 +130,16 @@ function hm_tor_message() {
         }
       })
       .success (function(response) {
-        jQuery('#hm_tor_msg').html(response + ' $msg_reload');
+        jQuery('#hm_tor_msg').html(response.msg + ' $msg_reload');
+        var revs = $revs_str;
+        for (var i = 0; i < revs.length; i++) {
+          for (var j = 0; j < response.deleted.length; j++) {
+            if (revs[i] == response.deleted[j]) {
+              jQuery('.post-revisions tr:eq(' + (i+1) + ')').css('text-decoration', 'line-through');
+              jQuery('.post-revisions tr:eq(' + (i+1) + ') :radio').attr('disabled', 'disabled');
+            }
+          }
+        }
       })
       .error (function() {
         jQuery('#hm_tor_msg').html('$msg_error');
@@ -133,28 +150,40 @@ function hm_tor_message() {
 </script>
 
 JQSRC;
-  echo $src;
-}
+    echo $src;
+  }
 
-function hm_tor_do_ajax() {
+  function hm_tor_do_ajax() {
   
-  $posts  = explode( "-", $_REQUEST['posts'] );
+    $posts  = explode( "-", $_REQUEST['posts'] );
 
-  if ( check_ajax_referer( 'hm-rto-delete-' . $_REQUEST['posts'], 'security', false ) ) {
-    $counter = 0;
-    foreach ( $posts as $revid ) {
-      if ( current_user_can( 'edit_post', $revid ) ) {
-        wp_delete_post_revision( $revid );
-        $counter++;
+    if ( check_ajax_referer( 'hm-rto-delete-' . $_REQUEST['posts'], 'security', false ) ) {
+      $deleted = array();
+      foreach ( $posts as $revid ) {
+        // Without the 'get_post' check, WP makes warnings.
+        if ( get_post($revid) && current_user_can( 'edit_post', $revid ) ) {
+          if ( wp_delete_post_revision( $revid ) ) {
+            array_push( $deleted, $revid );
+          }
+        }
       }
-      else {
-      }
+      echo json_encode( array(
+        "result" => "success", 
+        "msg" => sprintf( _n( '%s revision removed.', '%s revisions removed.', count( $deleted ), 'thin-out-revisions' ), count( $deleted ) ),
+        "deleted" => $deleted
+      ));
     }
-    printf( _n( '%s revision removed.', '%s revisions removed.', $counter, 'thin-out-revisions' ), $counter );
-  }
-  else {
-    echo __( "Wrong session. Unable to process.", 'thin-out-revisions' );
-  }
+    else {
+      echo json_encode( array(
+       "result" => "error",
+       "msg" => __( "Wrong session. Unable to process.", 'thin-out-revisions' )
+      ));
+    }
 
-  die();
-}
+    die();
+  }
+  
+} // end of class HM_TOR_Plugin_Loader
+
+$hm_tor_plugin_loader = new HM_TOR_Plugin_Loader();
+
